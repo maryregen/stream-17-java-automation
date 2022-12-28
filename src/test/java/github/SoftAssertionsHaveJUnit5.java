77e1e825a6be91d177a06db9e3421899f478070c
maryregen <maryregen@yandex.ru>
2022-12-28 20:02:52 +0400

    Removed excess imports

--- src/test/java/github/SoftAssertionsHaveJUnit5.java
+++ src/test/java/github/SoftAssertionsHaveJUnit5.java
@@ -1,12 +1,8 @@
 package github;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsHaveJUnit5 {
