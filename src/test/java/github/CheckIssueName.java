48bb7e380e85965d9d298dc80dfbd0be1fac5cb8
maryregen <maryregen@yandex.ru>
2023-01-25 17:46:37 +0400

    Added tests with using 1) listener, 2) step, 3) annotation

--- src/test/java/github/CheckIssueName.java
+++ src/test/java/github/CheckIssueName.java
@@ -3,7 +3,6 @@ package github;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
